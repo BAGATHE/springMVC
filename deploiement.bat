f870f9e62e9215a941525c02dace0e30ad7f2c35
Emadaly <lolemadaly@gmail.com>
2025-02-17 08:52:01 +0300

    modification deployement

--- deploiement.bat
+++ deploiement.bat
@@ -8,7 +8,7 @@ set "classes=E:\Licence_3\Semestre-5\M.Niaina\SpringMVC\Framework\classes"
 set "CLASSPATH=E:\Licence_3\Semestre-5\M.Niaina\SpringMVC\Test\lib"
 set "libParanamer=E:\Licence_3\Semestre-5\M.Niaina\SpringMVC\Framework\lib\paranamer-2.8.jar"
 set "libGson=E:\Licence_3\Semestre-5\M.Niaina\SpringMVC\Framework\lib\gson-2.8.2.jar"
-set "jarName=sprint14-2658"
+set "jarName=sprint15-2658"
 
 :: Créer les répertoires temporaires si nécessaires
 if not exist "%temp_src%" mkdir "%temp_src%"
